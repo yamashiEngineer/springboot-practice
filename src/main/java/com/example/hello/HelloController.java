f5b2b9c3e31bccfafab2e14b0ac8d14372e67b9e
Takaomi Yamashita <ty.8479@icloud.com>
2025-10-28 00:02:19 +0900

    データベースから値を取得する

--- src/main/java/com/example/hello/HelloController.java
+++ src/main/java/com/example/hello/HelloController.java
@@ -1,13 +1,18 @@
 package com.example.hello;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @Controller
 public class HelloController {
+
+  @Autowired
+  private HelloService service;
 
   @GetMapping("/hello")
   public String getHello() {
@@ -20,6 +25,16 @@ public class HelloController {
     model.addAttribute("sample", str);
 
     return "hello/response";
+  }
+
+  @PostMapping("/hello/db")
+  public String postDbRequest(@RequestParam("text2") String id, Model model) {
+
+    Employee employee = service.getEmployee(id);
+
+    model.addAttribute("employee", employee);
+
+    return "hello/db";
   }
 
 }
---
+++ src/main/resources/data.sql
@@ -0,0 +1,8 @@
+INSERT INTO employee (id, name, age)
+VALUES ('1', 'Alice', '30');
+INSERT INTO employee (id, name, age)
+VALUES ('2', 'Bob', '25');
+INSERT INTO employee (id, name, age)
+VALUES ('3', 'Charlie', '35');
+INSERT INTO employee (id, name, age)
+VALUES ('4', 'Diana', '28');
