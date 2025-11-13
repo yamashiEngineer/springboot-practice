be0694142523c5a92f028da050b25f17d498468d
Takaomi Yamashita <ty.8479@icloud.com>
2025-11-13 23:21:07 +0900

    バインド失敗時にエラーメッセージを表示する

--- src/main/java/com/example/controller/SignupController.java
+++ src/main/java/com/example/controller/SignupController.java
@@ -6,6 +6,7 @@ import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -34,8 +35,17 @@ public class SignupController {
     return "user/signup";
   }
 
+  // ユーザー登録処理
   @PostMapping("/signup")
-  public String postSignup(@ModelAttribute SignupForm form) {
+  public String postSignup(Model model, Locale locale,
+      @ModelAttribute SignupForm form,
+      BindingResult bindingResult) {
+
+    // 入力値チェック
+    if (bindingResult.hasErrors()) {
+      // NG: ユーザー登録画面に戻ります
+      return getSignup(model, locale, form);
+    }
 
     log.info(form.toString());
 
