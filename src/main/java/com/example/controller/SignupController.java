7e8c7841454e009c06a51efdd0570660e12d5c69
Takaomi Yamashita <ty.8479@icloud.com>
2025-11-16 23:10:10 +0900

    バリデーションの実行順序の設定

--- src/main/java/com/example/controller/SignupController.java
+++ src/main/java/com/example/controller/SignupController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import com.example.applicationservice.UserApplicationService;
+import com.example.form.GroupOrder;
 import com.example.form.SignupForm;
 
 import lombok.extern.slf4j.Slf4j;
@@ -39,7 +40,7 @@ public class SignupController {
   // ユーザー登録処理
   @PostMapping("/signup")
   public String postSignup(Model model, Locale locale,
-      @ModelAttribute @Validated SignupForm form,
+      @ModelAttribute @Validated(GroupOrder.class) SignupForm form,
       BindingResult bindingResult) {
 
     // 入力値チェック
