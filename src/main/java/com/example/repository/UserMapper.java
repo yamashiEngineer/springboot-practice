0f15d7a95235d0b9b24233ab51a50ae62cd62564
Takaomi Yamashita <ty.8479@icloud.com>
2025-12-02 22:54:12 +0900

    記述漏れ（inputタグによるidの付与）対応

--- src/main/java/com/example/repository/UserMapper.java
+++ src/main/java/com/example/repository/UserMapper.java
@@ -18,8 +18,8 @@ public interface UserMapper {
   // ユーザー取得（1件）
   public MUser findOne(String userId);
 
-  // ユーザー更新
-  public int updateOne(@Param("userId") String userId,
+  // ユーザー更新(1件)
+  public void updateOne(@Param("userId") String userId,
       @Param("password") String password,
       @Param("userName") String userName);
 
