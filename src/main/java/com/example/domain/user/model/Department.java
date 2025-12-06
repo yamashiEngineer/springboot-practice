10799a96c089e895fc84acc6952360e1ed6e013f
Takaomi Yamashita <ty.8479@icloud.com>
2025-12-06 22:47:53 +0900

    ユーザー詳細画面に部署名を表示

---
+++ src/main/java/com/example/domain/user/model/Department.java
@@ -0,0 +1,12 @@
+package com.example.domain.user.model;
+
+import lombok.Data;
+
+@Data
+public class Department {
+  /** 部署ID */
+  private Integer departmentId;
+
+  /** 部署名 */
+  private String departmentName;
+}
