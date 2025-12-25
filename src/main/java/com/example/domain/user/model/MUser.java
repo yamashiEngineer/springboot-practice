480fb3f168c9002da3f2f2b10607f06f6bd3869e
Takaomi Yamashita <ty.8479@icloud.com>
2025-12-25 23:38:58 +0900

    JPAの基本的な使い方

--- src/main/java/com/example/domain/user/model/MUser.java
+++ src/main/java/com/example/domain/user/model/MUser.java
@@ -1,13 +1,19 @@
 package com.example.domain.user.model;
 
 import java.util.Date;
-
 import java.util.List;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.Transient;
 import lombok.Data;
 
 @Data
+@Entity
+@Table(name = "m_user")
 public class MUser {
+  @Id
   private String userId;
   private String password;
   private String userName;
@@ -16,6 +22,8 @@ public class MUser {
   private Integer gender;
   private Integer departmentId;
   private String role;
+  @Transient
   private Department department;
+  @Transient
   private List<Salary> salaryList;
 }
---
+++ src/main/java/com/example/domain/user/service/impl/UserServiceImpl2.java
@@ -0,0 +1,84 @@
+package com.example.domain.user.service.impl;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Primary;
+import org.springframework.dao.DataAccessException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import com.example.domain.user.model.MUser;
+import com.example.domain.user.service.UserService;
+import com.example.repository.UserRepository;
+
+import jakarta.transaction.Transactional;
+
+@Service
+@Primary
+public class UserServiceImpl2 implements UserService {
+
+  @Autowired
+  private UserRepository repository;
+
+  @Autowired
+  private PasswordEncoder encoder;
+
+  // ユーザー登録
+  @Transactional
+  @Override
+  public void signup(MUser user) {
+    // 存在チェック
+    boolean exists = repository.existsById(user.getUserId());
+    if (exists) {
+      throw new DataAccessException("ユーザーIDが既に存在しています。") {
+      };
+    }
+
+    user.setDepartmentId(1); // デフォルトの部署IDを設定（例: 1）
+    user.setRole("Role_GENERAL"); // デフォルトの権限を設定
+
+    // パスワードを暗号化
+    String rawPassword = user.getPassword();
+    user.setPassword(encoder.encode(rawPassword));
+
+    // insert
+    repository.save(user);
+  }
+
+  // ユーザー取得
+  @Override
+  public List<MUser> getUsers(MUser user) {
+    return repository.findAll();
+  }
+
+  // ユーザー取得（1件）
+  @Override
+  public MUser getUserOne(String userId) {
+    Optional<MUser> option = repository.findById(userId);
+    MUser user = option.orElse(null);
+    return user;
+  }
+
+  // ユーザー更新（1件）
+  @Transactional
+  @Override
+  public void updateUserOne(String userId, String password, String userName) {
+  }
+
+  // ユーザー削除（1件）
+  @Transactional
+  @Override
+  public void deleteUserOne(String userId) {
+    repository.deleteById(userId);
+  }
+
+  // ログインユーザー取得
+  @Override
+  public MUser getLoginUser(String userId) {
+    Optional<MUser> option = repository.findById(userId);
+    MUser user = option.orElse(null);
+    return user;
+  }
+}
